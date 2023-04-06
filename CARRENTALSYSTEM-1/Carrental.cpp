f9e32f7cc95f47b25cadadb23be46350ca7b420b
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2023-04-06 14:31:41 -0600

    Add files via upload

--- CARRENTALSYSTEM-1/Carrental.cpp
+++ CARRENTALSYSTEM-1/Carrental.cpp
@@ -4,44 +4,51 @@
 //using std namespace will make it so yoiu can say cout instead of std::cout
 using namespace std;
 
-int main() {
-	
+int main() 
+{	
 	string choiceone;
 	string choicetwo;
+	string choiceOneR; // the r stands for return in these two variables
+	string choiceTwoR;
 while (true) {
-	 cout << "Type R to go back to the begining at any time\n";
-	 cout << "Are you are customer or employee?\n Type 'c' for customer and 'e' for employee\n";
+	 cout << "\nType R to go back to the begining at any time\n";
+	 cout << "Are you are customer or employee?\n Type 'Customer' for customer and 'Employee' for employee\n";
 	 getline (cin, choiceone);
-		if (choiceone == "e")
+		if (choiceone == "Employee")
 		{
 			cout << submenue();
-			
+			choiceone = choiceOneR;
 		}
-		else if (choiceone == "c"){
-			cout << "Are returning a car or renting a car?\n Type a for returning a car and b for renting a car.\n";
+		else if (choiceone == "R")
+		{
+			cout << "Starting over";
+			choicetwo = choiceTwoR;
+		}
+		else if (choiceone == "Customer"){
+			cout << "Are returning a car or renting a car?\n Type Returning for returning a car and Renting for renting a car.\n";
 			getline (cin, choicetwo);
-				if(choicetwo == "b")
+				if(choicetwo == "Renting")
 				{
 					submenure();
 				}
-				else if(choicetwo == "a")
+				else if(choicetwo == "Returning")
 				{
 					submenurn();
 				}
 				else {
-					cout << "That is not a valid letter";
+					cout << "That is not valid";
 				}
+				choicetwo = choiceTwoR;
 		}
 			else
 			{
 				cout << "Please try again";
+				choicetwo = choiceTwoR;
 			}
 		}
 		
 		cout << "Goodbye\n";
-		
-
-	}
+}
 
 
 	 
